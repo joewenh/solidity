c78fcbf3a4ebda362e53ecf5495fc55205ef79ec
joewenh <joe_wenh@163.com>
2026-01-02 00:53:53 +0800

    2222

--- tests/FundMe.sol
+++ tests/FundMe.sol
@@ -1,3 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+contract FundMe {
+    mapping(address => uint256) public fundersToAmount;
+
+    uint256 MINIMUM_VALUE = 1 * 10 ** 18;
+
+    function fund() external payable {
+        require(msg.value >= MINIMUM_VALUE, "Didn't send enough!");
+        fundersToAmount[msg.sender] = msg.value;
+    }
+}
