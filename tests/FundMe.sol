a75d6af094f21fb73064e2876bbea3afe248ecb2
joewenh <joe_wenh@163.com>
2026-01-02 11:12:22 +0800

    321

--- tests/FundMe.sol
+++ tests/FundMe.sol
@@ -2,12 +2,36 @@
 pragma solidity ^0.8.20;
 
 contract FundMe {
+
+    AggregatorV3Interface internal dataFeed;
+
     mapping(address => uint256) public fundersToAmount;
 
     uint256 MINIMUM_VALUE = 1 * 10 ** 18;
+
+
+    constructor()
 
     function fund() external payable {
         require(msg.value >= MINIMUM_VALUE, "Didn't send enough!");
         fundersToAmount[msg.sender] = msg.value;
     }
+
+
+
+    function getChainlinkDataFeedLatestAnswer() public view returns (int256) {
+        // prettier-ignore
+        (
+        /* uint80 roundId */
+        ,
+        int256 answer,
+        /*uint256 startedAt*/
+        ,
+        /*uint256 updatedAt*/
+        ,
+        /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return answer;
+    }
+
 }
